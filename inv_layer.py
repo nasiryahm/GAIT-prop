b2addc65b02b438562c3d9eef4dea84d0222edc3
Nasir Ahmad <nasiryahm@gmail.com>
2020-11-10 11:36:21 +0100

    Shifting to forward bias notation.

--- inv_layer.py
+++ inv_layer.py
@@ -28,6 +28,7 @@ class InvLayer:
         self.forward_weight_matrix = xp.random.randn(nb_units, nb_units)
         # Applying a scaling to the weight matrix (Xavier init)
         self.forward_weight_matrix *= xp.sqrt(2/(self.forward_weight_matrix.shape[0]+self.forward_weight_matrix.shape[1]))
+        self.forward_biases = xp.zeros(nb_units)
 
         # If we want an orthogonal init, we have to correct the shape
         if orthogonal_init:
@@ -37,7 +38,6 @@ class InvLayer:
 
         self.inverse_matrix = None
         self.update_exact_inverse() # Inverse defined here
-        self.biases = xp.zeros(nb_units)
     
         self.first_moment_factor = 0.9
         self.second_moment_factor = 0.99
@@ -55,15 +55,15 @@ class InvLayer:
         return self.forward(data)
 
     def forward(self, data):
-        weight_transform = xp.einsum('ij, ni -> nj', self.forward_weight_matrix, data) + self.biases
+        weight_transform = xp.einsum('ij, ni -> nj', self.forward_weight_matrix, data) + self.forward_biases
         if self.linear:
             return weight_transform
         return self.transfer_func(weight_transform)
 
     def inverse(self, data):
         if self.linear:
-            return xp.einsum('ji, nj -> ni', self.inverse_matrix, data - self.biases)
-        return xp.einsum('ji, nj -> ni', self.inverse_matrix, self.transfer_inverse_func(data) - self.biases)
+            return xp.einsum('ji, nj -> ni', self.inverse_matrix, data - self.forward_biases)
+        return xp.einsum('ji, nj -> ni', self.inverse_matrix, self.transfer_inverse_func(data) - self.forward_biases)
 
     def update_exact_inverse(self):
         self.inverse_matrix = xp.linalg.inv(self.forward_weight_matrix)
@@ -79,21 +79,21 @@ class InvLayer:
         else:
             self.forward_weight_matrix += learning_rate*direction
 
-    def update_biases(self, direction, learning_rate):
+    def update_forward_biases(self, direction, learning_rate):
         if self.adaptive:
             self.first_moment_factor_biases = (1.0 - self.first_moment_factor)*direction + self.first_moment_factor*self.first_moment_factor_biases
             self.second_moment_factor_biases = (1.0 - self.second_moment_factor)*direction**2 + self.second_moment_factor*self.second_moment_factor_biases
 
             learning_rate = (learning_rate / (1e-8 + xp.sqrt(self.second_moment_factor_biases)))
 
-            self.biases += learning_rate*self.first_moment_factor_biases
+            self.forward_biases += learning_rate*self.first_moment_factor_biases
         else:
-            self.biases += learning_rate*direction
+            self.forward_biases += learning_rate*direction
 
     def save_params(self, path):
         xp.save(path + "forward_weights.npy", self.forward_weight_matrix)
-        xp.save(path + "biases.npy", self.biases)
+        xp.save(path + "forward_biases.npy", self.forward_biases)
 
     def load_params(self, path):
         self.forward_weight_matrix = xp.load(path + "forward_weights.npy")
-        self.biases = xp.load(path + "biases.npy")
+        self.forward_biases = xp.load(path + "forward_biases.npy")
